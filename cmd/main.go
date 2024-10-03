5db1431e0effbe85a5549fd145d6c6fc8cc4be6c
zhangtiantian <zhangtiantian@haohaozhu.com>
2024-10-03 10:54:49 +0800

    配置读取

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,19 @@
 package main
 
 import (
+	"crypto/tls"
+	"crypto/x509"
 	"flag"
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"io/ioutil"
 	"log"
 	"ming/internal/db"
 	anime_handler "ming/internal/handlers/anime"
 	anime_service "ming/internal/service/anime"
 	"ming/pkg/config"
 	"ming/pkg/logger"
+	"net/http"
 )
 
 func main() {
@@ -36,6 +40,51 @@ func main() {
 	//}
 
 	// 初始化路由
+	//r := gin.Default()
+	//animeService := anime_service.NewAnimeService()
+	//animeHandler := anime_handler.NewAnimeHandler(animeService)
+	//
+	//anime := r.Group("/anime")
+	//{
+	//	anime.GET("/list", animeHandler.GetAnimeList)
+	//	anime.GET("/detail/:anime_id", animeHandler.GetAnimeByID)
+	//}
+
+	// 1. 加载证书文件
+	certFile := "/etc/ssl/certs/aliyun-root-ca.crt"
+	certBytes, err := ioutil.ReadFile(certFile)
+	if err != nil {
+		log.Fatalf("Failed to read certificate file: %v", err)
+	}
+
+	// 2. 解析证书
+	rootCAs := x509.NewCertPool()
+	if !rootCAs.AppendCertsFromPEM(certBytes) {
+		log.Fatalf("Failed to append cert to pool")
+	}
+
+	// 3. 创建 TLS 配置
+	tlsConfig := &tls.Config{
+		RootCAs: rootCAs,
+		MinVersion:  tls.VersionTLS12,
+		CipherSuites: []uint16{
+			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_RSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
+		},
+	}
+
+	//// 4. 初始化 Gin 框架
+	//router := gin.Default()
+	//
+	//// 5. 添加路由
+	//router.GET("/", func(c *gin.Context) {
+	//	c.JSON(http.StatusOK, gin.H{
+	//		"message": "Hello, world!",
+	//	})
+	//})
+
 	r := gin.Default()
 	animeService := anime_service.NewAnimeService()
 	animeHandler := anime_handler.NewAnimeHandler(animeService)
@@ -46,9 +95,24 @@ func main() {
 		anime.GET("/detail/:anime_id", animeHandler.GetAnimeByID)
 	}
 
-	fmt.Println("Listening on :443...")
-	logger.Info("Server started.")
-	if err := r.RunTLS(":443", "/ssl/cert.pem", "/ssl/cert.key"); err != nil {
-		log.Fatalf("Failed to start server: %v", err)
+	// 6. 创建 HTTP 服务器实例
+	server := &http.Server{
+		Addr:      ":443", // 你可以更改端口号
+		Handler:   r,
+		TLSConfig: tlsConfig,
 	}
+
+	// 7. 启动 HTTPS 服务
+	log.Println("Starting HTTPS server...")
+	err = server.ListenAndServeTLS("", "")
+	if err != nil && err != http.ErrServerClosed {
+		log.Fatalf("Failed to start HTTPS server: %v", err)
+	}
+
+
+	//fmt.Println("Listening on :443...")
+	//logger.Info("Server started.")
+	//if err := r.RunTLS(":443", "/ssl/cert.pem", "/ssl/cert.key"); err != nil {
+	//	log.Fatalf("Failed to start server: %v", err)
+	//}
 }
